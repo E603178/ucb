57f87712fb3767faee45916966d2c28e1eb9c167
E603178 <jeroen.claes@ucb.com>
2015-12-11 16:35:00 +0100

    working on dynamicly adding list items to the main page

--- src/ISDemo/www/js/signin-controller.js
+++ src/ISDemo/www/js/signin-controller.js
@@ -1,138 +1,182 @@
 var ISDemo = ISDemo || {};
 
-ISDemo.SignInController = function () {
+ISDemo.SignInController = function() {
 
-    this.$signInPage = null;
-    this.$btnSubmit = null;
-    this.$txtUsername = null;
-    this.$txtPassword = null;
-    this.$chkKeepSignedIn = null;
-    this.$ctnErr = null;
-    this.mainMenuPageId = null;
+	this.$signInPage = null;
+	this.$btnSubmit = null;
+	this.$txtUsername = null;
+	this.$txtPassword = null;
+	this.$chkKeepSignedIn = null;
+	this.$ctnErr = null;
+	this.mainMenuPageId = null;
 };
 
-ISDemo.SignInController.prototype.init = function () {
-    this.$signInPage = $("#page-signin");
-    this.mainMenuPageId = "#page-eventlist";
-    this.$btnSubmit = $("#btn-submit", this.$signInPage);
-    this.$ctnErr = $("#ctn-err", this.$signInPage);
-    this.$txtUsername = $("#txt-username", this.$signInPage);
-    this.$txtPassword = $("#txt-password", this.$signInPage);
-    this.$chkKeepSignedIn = $("#chk-keep-signed-in", this.$signInPage);
+ISDemo.SignInController.prototype.init = function() {
+	this.$signInPage = $("#page-signin");
+	this.mainMenuPageId = "#page-eventlist";
+	this.$btnSubmit = $("#btn-submit", this.$signInPage);
+	this.$ctnErr = $("#ctn-err", this.$signInPage);
+	this.$txtUsername = $("#txt-username", this.$signInPage);
+	this.$txtPassword = $("#txt-password", this.$signInPage);
+	this.$chkKeepSignedIn = $("#chk-keep-signed-in", this.$signInPage);
 };
 
-ISDemo.SignInController.prototype.usernameIsValid = function (username) {
-	return username.length==7 
-	&&	(username.charAt(0).toUpperCase()=="U" 
-			|| username.charAt(0).toUpperCase()=="E" 
-			|| username.charAt(0).toUpperCase()=="T")
-	&& !(isNaN(username.substring(1, 7)));
+ISDemo.SignInController.prototype.usernameIsValid = function(username) {
+	return username.length == 7
+			&& (username.charAt(0).toUpperCase() == "U" || username.charAt(0).toUpperCase() == "E" || username
+					.charAt(0).toUpperCase() == "T") && !(isNaN(username.substring(1, 7)));
 };
 
-ISDemo.SignInController.prototype.resetSignInForm = function () {
+ISDemo.SignInController.prototype.resetSignInForm = function() {
 
-    var invisibleStyle = "bi-invisible",
-        invalidInputStyle = "bi-invalid-input";
+	var invisibleStyle = "bi-invisible", invalidInputStyle = "bi-invalid-input";
 
-    this.$ctnErr.html("");
-    this.$ctnErr.removeClass().addClass(invisibleStyle);
-    this.$txtUsername.removeClass(invalidInputStyle);
-    this.$txtPassword.removeClass(invalidInputStyle);
-    this.$txtUsername.val("");
-    this.$txtPassword.val("");
-    this.$chkKeepSignedIn.prop("checked", false);
+	this.$ctnErr.html("");
+	this.$ctnErr.removeClass().addClass(invisibleStyle);
+	this.$txtUsername.removeClass(invalidInputStyle);
+	this.$txtPassword.removeClass(invalidInputStyle);
+	this.$txtUsername.val("");
+	this.$txtPassword.val("");
+	this.$chkKeepSignedIn.prop("checked", false);
 };
 
-ISDemo.SignInController.prototype.onSignInCommand = function () {
+ISDemo.SignInController.prototype.onSignInCommand = function() {
 
-    var me = this,
-        Username = me.$txtUsername.val().trim(),
-        password = me.$txtPassword.val().trim(),
-        invalidInput = false,
-        invisibleStyle = "bi-invisible",
-        invalidInputStyle = "bi-invalid-input";
+	var me = this, username = me.$txtUsername.val().trim(), password = me.$txtPassword.val().trim(), invalidInput = false, invisibleStyle = "bi-invisible", invalidInputStyle = "bi-invalid-input";
 
-    // Reset styles.
-    me.$ctnErr.removeClass().addClass(invisibleStyle);
-    me.$txtUsername.removeClass(invalidInputStyle);
-    me.$txtPassword.removeClass(invalidInputStyle);
+	// Reset styles.
+	me.$ctnErr.removeClass().addClass(invisibleStyle);
+	me.$txtUsername.removeClass(invalidInputStyle);
+	me.$txtPassword.removeClass(invalidInputStyle);
 
-    // Flag each invalid field.
-    if (Username.length === 0) {
-        me.$txtUsername.addClass(invalidInputStyle);
-        invalidInput = true;
-    }
-    if (password.length === 0) {
-        me.$txtPassword.addClass(invalidInputStyle);
-        invalidInput = true;
-    }
+	// Flag each invalid field.
+	if (username.length === 0) {
+		me.$txtUsername.addClass(invalidInputStyle);
+		invalidInput = true;
+	}
+	if (password.length === 0) {
+		me.$txtPassword.addClass(invalidInputStyle);
+		invalidInput = true;
+	}
 
-    // Make sure that all the required fields have values.
-    if (invalidInput) {
-        me.$ctnErr.html("<p>Please enter all the required fields.</p>");
-        me.$ctnErr.addClass("bi-ctn-err").slideDown();
-        return;
-    }
+	// Make sure that all the required fields have values.
+	if (invalidInput) {
+		me.$ctnErr.html("<p>Please enter all the required fields.</p>");
+		me.$ctnErr.addClass("bi-ctn-err").slideDown();
+		return;
+	}
 
-    if (!me.usernameIsValid(Username)) {
-        me.$ctnErr.html("<p>Please enter a valid username.</p>");
-        me.$ctnErr.addClass("bi-ctn-err").slideDown();
-        me.$txtUsername.addClass(invalidInputStyle);
-        return;
-    }
-/*
-    $.mobile.loading("show");
+	if (!me.usernameIsValid(username)) {
+		me.$ctnErr.html("<p>Please enter a valid username.</p>");
+		me.$ctnErr.addClass("bi-ctn-err").slideDown();
+		me.$txtUsername.addClass(invalidInputStyle);
+		return;
+	}
 	
-    $.ajax({
-        type: 'POST',
-        url: ISDemo.Settings.signInUrl,
-        data: "username=" + username + "&password=" + password,
-        success: function (resp) {
+	$.mobile.loading("show");
 
-            $.mobile.loading("hide");
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "https://webmessaging-test.ucb.com:9233/ucbboothdemo/Login", true);
 
-            if (resp.success === true) {
-			*/
-                // Create session. 
-                var today = new Date();
-                var expirationDate = new Date();
-                expirationDate.setTime(today.getTime() + ISDemo.Settings.sessionTimeoutInMSec);
+	var sr = "<?xml version='1.0' encoding='UTF-8'?>";
+	sr += "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">";
+	sr += "<soap:Header>";
+	sr += "<wsse:Security xmlns:wsse=\"http:\//docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" soap:mustUnderstand=\"1\">";
+	sr += "<wsse:UsernameToken xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">";
+	sr += "<wsse:Username>" + username + "<\/wsse:Username>";
+	sr += "<wsse:Password Type=\"http:\//docs.oasis-open.org\/wss\/2004\/01\/oasis-200401-wss-username-token-profile-1.0#PasswordText\">"
+			+ password + "<\/wsse:Password>";
+	sr += "<\/wsse:UsernameToken>";
+	sr += "<\/wsse:Security>";
+	sr += "<\/soap:Header>";
+	sr += "<soap:Body xmlns:ns1=\"http://xmlns.oracle.com/bpel/mobile/Notificationlist\">";
+	sr += "<wsse:Username>" + username +"</wsse:Username>";
+	sr += "<wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">" + password + "</wsse:Password>";
+	sr += "<\/soap:Body>";
+	sr += "<\/soap:Envelope>";
 
-//                ISDemo.Session.getInstance().set({
-//                    userProfileModel: resp.extras.userProfileModel,
-//                    sessionId: resp.extras.sessionId,
-//                    expirationDate: expirationDate,
-//                    keepSignedIn:me.$chkKeepSignedIn.is(":checked")
-//                });
-                // Go to main menu.
-               $.mobile.navigate(me.mainMenuPageId);
-            /*    
-				return;
-            } else {
-                if (resp.extras.msg) {
-                    switch (resp.extras.msg) {
-                        case ISDemo.ApiMessages.DB_ERROR:
-                        // TODO: Use a friendlier error message below.
-                            me.$ctnErr.html("<p>Oops! ISDemo had a problem and could not log you on.  Please try again in a few minutes.</p>");
-                            me.$ctnErr.addClass("bi-ctn-err").slideDown();
-                            break;
-                        case ISDemo.ApiMessages.INVALID_PWD:
-                        case ISDemo.ApiMessages.USERNAME_NOT_FOUND:
-                            me.$ctnErr.html("<p>You entered a wrong username or password.  Please try again.</p>");
-                            me.$ctnErr.addClass("bi-ctn-err").slideDown();
-                            me.$txtUsername.addClass(invalidInputStyle);
-                            break;
-                    }
-                }
-            }
-        },
-        error: function (e) {
-            $.mobile.loading("hide");
-            console.log(e.message);
-            // TODO: Use a friendlier error message below.
-            me.$ctnErr.html("<p>Oops! ISDemo had a problem and could not log you on.  Please try again in a few minutes.</p>");
-            me.$ctnErr.addClass("bi-ctn-err").slideDown();
-        }
-    });
-*/
+	// Send the POST request
+	xmlhttp.setRequestHeader("Accept", "application/xml", "text/xml", "\*/\*");
+	xmlhttp.setRequestHeader("SOAPAction", "Login");
+	xmlhttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+	//xmlhttp.send(sr);
+	// send request
+	
+	$.mobile.loading("hide");
+	$.mobile.navigate(me.mainMenuPageId);
+	//Skip login for testing purposes. TODO remove for final deployment.
+	
+	
+	// ...
+	
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			if (xmlhttp.status == 200) {
+				$.mobile.loading("hide");
+				// Create session.
+				var today = new Date();
+				var expirationDate = new Date();
+				expirationDate.setTime(today.getTime() + ISDemo.Settings.sessionTimeoutInMSec);
+
+				 ISDemo.Session.getInstance().set({
+				 userProfileModel: resp.extras.userProfileModel,
+				 sessionId: resp.extras.sessionId,
+				 expirationDate: expirationDate,
+				 keepSignedIn:me.$chkKeepSignedIn.is(":checked"),
+				 username:username,
+				 password:password
+				 });
+				// Go to main menu.
+				$.mobile.navigate(me.mainMenuPageId);
+				
+				
+				
+			} else {
+				$.mobile.loading("hide");
+				alert('Error ' + xmlhttp.status);
+			}
+		}
+	}	
+	
+
+	/*
+	 * 
+	 * 
+	 * 
+	 * $.ajax({ type: 'POST', url: ISDemo.Settings.signInUrl, data: "username=" +
+	 * username + "&password=" + password, success: function (resp) {
+	 * 
+	 * $.mobile.loading("hide");
+	 * 
+	 * if (resp.success === true) {
+	 */
+	// Create session.
+	var today = new Date();
+	var expirationDate = new Date();
+	expirationDate.setTime(today.getTime() + ISDemo.Settings.sessionTimeoutInMSec);
+
+	 ISDemo.Session.getInstance().set({
+	 userProfileModel: resp.extras.userProfileModel,
+	 sessionId: resp.extras.sessionId,
+	 expirationDate: expirationDate,
+	 keepSignedIn:me.$chkKeepSignedIn.is(":checked")
+	 });
+	// Go to main menu.
+	$.mobile.navigate(me.mainMenuPageId);
+	/*
+	 * return; } else { if (resp.extras.msg) { switch (resp.extras.msg) { case
+	 * ISDemo.ApiMessages.DB_ERROR: // TODO: Use a friendlier error message
+	 * below. me.$ctnErr.html("<p>Oops! ISDemo had a problem and could not log
+	 * you on. Please try again in a few minutes.</p>");
+	 * me.$ctnErr.addClass("bi-ctn-err").slideDown(); break; case
+	 * ISDemo.ApiMessages.INVALID_PWD: case
+	 * ISDemo.ApiMessages.USERNAME_NOT_FOUND: me.$ctnErr.html("<p>You entered
+	 * a wrong username or password. Please try again.</p>");
+	 * me.$ctnErr.addClass("bi-ctn-err").slideDown();
+	 * me.$txtUsername.addClass(invalidInputStyle); break; } } } }, error:
+	 * function (e) { $.mobile.loading("hide"); console.log(e.message); // TODO:
+	 * Use a friendlier error message below. me.$ctnErr.html("<p>Oops! ISDemo
+	 * had a problem and could not log you on. Please try again in a few
+	 * minutes.</p>"); me.$ctnErr.addClass("bi-ctn-err").slideDown(); } });
+	 */
 };
