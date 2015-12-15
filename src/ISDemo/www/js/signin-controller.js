2aa8ea8c78099b47eb59f05fdc0acd4e981e341f
jeroen claes <jeroen.claes@ucb.com>
2015-12-15 18:03:01 +0100

    allowed xream ajax requests

--- src/ISDemo/www/js/signin-controller.js
+++ src/ISDemo/www/js/signin-controller.js
@@ -75,6 +75,7 @@ ISDemo.SignInController.prototype.onSignInCommand = function() {
 
 	$.mobile.loading("show");
 
+	$.support.cors=true;
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("POST", "https://webmessaging-test.ucb.com:9233/ucbboothdemo/Login", true);
 
